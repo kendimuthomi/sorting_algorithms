cb8f42adfda689b1d032d603f6f3e243ac305673
kendimuthomi <kendijackline42@gmail.com>
2022-10-11 06:27:43 +0000

    Bubble sort of an array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; ++i)
 	{
 		for (j = 0; j < size - i - 1; ++j)
