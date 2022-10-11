13002d533d4a47015dea89d30c4641f650f821a7
kendimuthomi <kendijackline42@gmail.com>
2022-10-11 07:41:47 +0000

    Sorting an array using selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,1 +1,38 @@
 #include "sort.h"
+
+/**
+ * swap - swaps the positions of two elements
+ * @a: first element
+ * @b: second element
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * selection_sort - uses selection sort to sort an array
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, index, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[index])
+				index = j;
+		}
+		swap(&array[index], &array[i]);
+		print_array(array, size);
+	}
+}
