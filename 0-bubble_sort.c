cb4fccfe1b8135ef37d0b72cc224637ce1a0f070
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2022-10-11 08:59:15 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,10 @@
 #include "sort.h"
+
+/**
+*bubble_sort - sorts an array using bubble sort
+*@array: array to be sorted
+*@size: size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+    
